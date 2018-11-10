5aac159b3960c46b7269a5355e4dd4e05a913a44
Brian Rice <brice@bigloops.com>
2018-11-09 19:14:55 -0800

    ready to rumble

--- generate-wordfind.py
+++ generate-wordfind.py
@@ -2,23 +2,23 @@
 '''
 Geometry:
   
-    0 1 2 ... width-1
    0
    1
    2
    ...
    height-1
+             0 1 2 ... width-1
 '''
 
 directionlist = ( 
         (1,0),          # forwards
-        (1,1),          # diagonal up forwards
-        (0,1),          # up
-        (-1,1),         # diagonal up backwards
+        (1,-1),          # diagonal up forwards
+        (0,-1),          # up
+        (-1,-1),         # diagonal up backwards
         (-1,0),         # backwards
-        (-1,-1),        # diagonal down backwards
-        (0,-1),         # down
-        (1,-1)          # diagonal down forwards
+        (-1,1),        # diagonal down backwards
+        (0,1),         # down
+        (1,1)          # diagonal down forwards
 )
 
 def solve(puzzlestate, wordlist):
@@ -28,11 +28,10 @@ def solve(puzzlestate, wordlist):
       return True
     for i, tryword in enumerate(wordlist):
       rest_of_wordlist = [wordlist[x] for x in range(len(wordlist)) if x != i ]
-      for trydir in directionlist:
-        for tryloc in possible_word_starts(puzzlestate,tryword,trydir):
-          if word(puzzlestate,tryword,tryloc):
-            puzzlestate2 = copy(puzzlestate)
-            inscribe_word(puzzlestate2,tryword,tryloc)
+      for trydirection in directionlist:
+        for trylocation in possible_word_starts(puzzlestate,tryword,trydirection):
+         puzzlestate2 = copy(puzzlestate)
+         if inscribe_word(puzzlestate2,tryword,trylocation):
             if solve(puzzlestate2,rest_of_wordlist):
               return True
     return False
@@ -54,6 +53,13 @@ def main():
   except IOError:
   	print("%s: couldn't open wordlistfile %s\n" % (sys.argv[0], wordlistfile))
   	sys.exit(2)
+
+  p = puzzlestate(height,width)
+
+  if solve(p,wordlist):
+    print("success")
+  else:
+    print("failure")
 
 
 if __name__ == "__main__":
