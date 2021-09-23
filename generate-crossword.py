6baefba2251d5855e78bd921483b32be2a10351d
Brian Rice <brice@bigloops.com>
2021-09-22 20:50:57 -0700

    closing in on recursion

--- generate-crossword.py
+++ generate-crossword.py
@@ -1,77 +1,52 @@
 #!/usr/bin/env python3
 '''
-generate-wordfind.py: create a hidden-word puzzle
+generate-crossword.py: create a hidden-word puzzle
 
-usage: generate-wordfind.py height width wordlistfile
-
-Geometry:
-  
-   0
-   1
-   2
-   ...
-   height-1
-             0 1 2 ... width-1
-'''
+usage: generate-crossword.py puzzlefile.json
 
 import sys
 import copy
 
 from puzzlestate import Puzzlestate
+from randomword import Randomword
 
-def pack_with_words(p, wordlist):
-  if not wordlist:
-    # Hey! That means we packed all the possible words into the puzzle.
-    p.export_puzzlestate()
+def solve(p):
+  r = Randomword(0)
+
+  #p.export_puzzlestate()
+  (direction, cluenumber, cluelength) = p.fetch_random_unsolved_clue()
+  if direction is None:
+    # puzzle is solved! no more unsolved clues
     return p
+    
+  already_tried_words = list()
+  while True:
+    tryword = r.randomword(direction, cluenumber, cluelength, 
+                           already_tried_words,
+                           p)
+    if tryword is None:
+      # no words in the dictionary fit that haven't been tried
+      return None
+      
+    p2 = p.inscribe_word(tryword, direction, cluelength)
+    # the inscribe_word method guarantees it fits
 
-    for i, tryword in enumerate(wordlist):
-      rest_of_wordlist = [wordlist[x] for x in range(len(wordlist)) if x != i ]
-      for trydirection in p.directionlist:
-        for trylocation in p.possible_word_starts(tryword,trydirection):
-          p2 = p.inscribe_word(tryword,trylocation)
-          if p2 is None:
-            # oh well, that means we were not able to inscribe this word at this location in this direction.
-            # try the next one!
-            continue
-          else:
-            # cool! let's recurse.
-            if pack_with_words(p2,rest_of_wordlist):
-              return p2
+    already_tried_words.append(tryword)
 
-    # if we make it here, that means that none of the words in wordlist can be inscribed.
-    print("none of the words in wordlist {} can be inscribed.".format(wordlist))
-    return None
+    p3 = solve(p2)
+    if p3 is not None:
+      break
 
+  return p3
+    
 def main():
-  if len(sys.argv) != 4 or int(sys.argv[1]) < 1 or int(sys.argv[2]) < 1:
-    print("usage: {} height width wordlistfile".format(sys.argv[0]))
+  if len(sys.argv) != 2:
+    print("usage: {} puzzlefile.json".format(sys.argv[0]))
     sys.exit(1)
-  
-  height = int(sys.argv[1])
-  width = int(sys.argv[2])
-  wordlistfile = sys.argv[3]  
 
-  wordlist = list()
- 
-  try:
-    with open(wordlistfile,'rb') as f:
-      wordlist2 = f.readlines()
-  except IOError:
-    print("{}: couldn't open wordlistfile {}".format(sys.argv[0], wordlistfile))
-    sys.exit(2)
+  p = Puzzlestate.fromjsonfile(sys.argv[1])
 
-  wordlist = [str(x.decode('utf-8').strip()) for x in wordlist2]
-  longestword = max(wordlist,key=len)
-  maxlen = len(longestword)
-
-  if maxlen > height and maxlen > width:
-    print("{}: wordlistfile {} contains a word {} that won't fit in the puzzle".format(sys.argv[0], wordlistfile, longestword))
-    sys.exit(3)
-
-  p = Puzzlestate(height,width)
-
-  pack_with_words(p,wordlist)
+  solve(p)
   p.print()
 
 if __name__ == "__main__":
